3bef01be9a2ec908f87e2f23d027f0e093ba1f82
vincentbaylon <vincentbaylon@gmail.com>
2021-07-02 16:21:31 -0700

    finished project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.querySelector('#create-task-form').addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  let newTask = e.target['new-task-description'].value
+  addTask(newTask)
+})
+
+function addTask(task) {
+  let li = document.createElement('li')
+  let ul = document.querySelector('#tasks')
+  let btn = document.createElement('button')
+
+  li.textContent = task
+  btn.textContent = 'X'
+
+  li.append(btn)
+  ul.append(li)
+
+  btn.addEventListener('click', () => {
+    li.remove()
+  })
+}
